cb3e27ced522c257a1bcca8f76cff7a1b8afec5f
YajatKaul <68262974+yajatkaul@users.noreply.github.com>
2024-10-15 10:51:30 +0530

    new file:   frontend/src/app/page.tsx

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
+
+const Page = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/home");
+  }, []);
+  return <div></div>;
+};
+
+export default Page;
