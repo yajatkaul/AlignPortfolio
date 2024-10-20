06215e44203cc780e92a72ef36ba46fc0bd5c682
YajatKaul <68262974+yajatkaul@users.noreply.github.com>
2024-10-20 08:18:33 +0530

    modified:   backend/routes/auth.routes.js

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,15 +1,9 @@
 import express from "express";
-import {
-  getOTP,
-  login,
-  logout,
-  signup,
-} from "../controller/auth.controller.js";
+import { getOTP, login, logout } from "../controller/auth.controller.js";
 import checkAuth from "../middleware/checkAuth.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
 router.post("/login", login);
 router.get("/logout", logout);
 router.get("/getotp/:number", getOTP);
